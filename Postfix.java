707c64e71a7ccbc34eac8da38cbc935108d88c76
Caio Barreto <Caio.BezerraLondresBarreto@windriver.com>
2022-09-27 21:52:17 -0300

    feature(master): token implementation

--- Postfix.java
+++ Postfix.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
+
+import stacker.rpn.lexer.Token;
+import stacker.rpn.lexer.TokenType;
 
 public class Postfix {
     public static boolean isNumeric(String str) {
@@ -13,21 +15,44 @@ public class Postfix {
         }
     }
 
-    public static String readFile(String filename) {
-        String expression = "";
+    public static TokenType getTokenType(String tokenType) {
+        if (isNumeric(tokenType)) {
+            return TokenType.NUM;
+        } else if (Objects.equals(tokenType, "+")) {
+            return TokenType.PLUS;
+        } else if (Objects.equals(tokenType, "-")) {
+            return TokenType.MINUS;
+        } else if (Objects.equals(tokenType, "*")) {
+            return TokenType.STAR;
+        } else if (Objects.equals(tokenType, "/")) {
+            return TokenType.SLASH;
+        } else {
+            return TokenType.EOF;
+        }
+    }
+
+    public static List<Token> readFile(String filename) {
+        List<Token> tokenArray = new ArrayList<Token>();
         try {
             File myObj = new File(filename);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                expression = expression + data;
+                TokenType tokenType = getTokenType(data);
+                if (tokenType == TokenType.EOF) {
+                    throw new Exception("Error: Unexpected character: " + data);
+                } else {
+                    tokenArray.add(new Token(tokenType, data));
+                }
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return expression;
+        return tokenArray;
     }
 
     public static int operation(Integer number1, Integer number2, String operator) {
@@ -43,23 +68,23 @@ public class Postfix {
         }
     }
 
-    public static int postfix(String expression) {
+    public static int postfix(List<Token> expression) {
         Stack stk = new Stack();
-        for (int charIndex = 0; charIndex < expression.length(); charIndex++) {
-            String str = String.valueOf(expression.charAt(charIndex));
-            if (isNumeric(str)) {
-                stk.push(Integer.parseInt(str));
+        for (int itemIndex = 0; itemIndex < expression.size(); itemIndex++) {
+            Token token = expression.get(itemIndex);
+            if (token.type == TokenType.NUM) {
+                stk.push(Integer.parseInt(token.lexeme));
             } else {
                 Integer number1 = (Integer) stk.pop();
                 Integer number2 = (Integer) stk.pop();
-                stk.push(operation(number1, number2, str));
+                stk.push(operation(number1, number2, token.lexeme));
             }
         }
         return (Integer) stk.pop();
     };
 
     public static void main(String[] args) {
-        String expression = readFile("example.txt");
+        List<Token> expression = readFile("example.txt");
         System.out.println(postfix(expression));
     }
 };
